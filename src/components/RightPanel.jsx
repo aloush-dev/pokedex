310d8143b7be3c529e51a0ff275542fa5863af78
itsaliabdallah <itsaliabdallah@gmail.com>
2022-10-24 16:52:06 +0100

    added panels

--- src/components/RightPanel.jsx
+++ src/components/RightPanel.jsx
@@ -2,15 +2,23 @@ import styles from "../styles/rightpanel.module.css";
 import { RightPanelBoxes } from "./RightPanelBoxes";
 
 export const RightPanel = () => {
-  return (
-    <div className={styles.rightside}>
-      <div className={styles.topcontainer}>
-        <div className={styles.top}></div>
+  const panels = [];
+
+  {
+    for (let i = 0; i < 10; i++) {
+      panels.push(<RightPanelBoxes />);
+    }
+
+    return (
+      <div className={styles.rightside}>
+        <div className={styles.topcontainer}>
+          <div className={styles.top}></div>
+        </div>
+        <div className={styles.middlecontainer}>
+          {panels}
+        </div>
+        <div className={styles.bottomcontainer}></div>
       </div>
-      <div className={styles.middlecontainer}>
-        <RightPanelBoxes />
-      </div>
-      <div className={styles.bottomcontainer}></div>
-    </div>
-  );
+    );
+  }
 };
